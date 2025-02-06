455505ac03a9a94a2336109b5d9aef6f0f9ebc8e
d3k0d4nce <kishko.2003@list.ru>
2025-02-06 18:35:25 +0400

    Подключение серверной части для авторизации и регистрации

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,25 +1,93 @@
 // eslint-disable-next-line no-unused-vars
-import React, {useState} from 'react';
-import {Button, Input} from 'antd';
+import React, {useEffect, useState} from 'react';
+import {Button, Input, message} from 'antd';
 import {Link, useNavigate} from 'react-router-dom';
 import './styles.css';
+import axios from 'axios';
 
 const AuthPage = () => {
     const [isRegistration, setIsRegistration] = useState(false);
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState(''); // Для авторизации
+    const [password, setPassword] = useState('');
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [image, setImage] = useState(null);
     const navigate = useNavigate();
 
     const handleToggle = () => {
         setIsRegistration(!isRegistration);
     };
 
-    const handleLogin = () => {
-        console.log('Авторизация');
-        navigate('/statistics');
+    useEffect(() => {
+        if (!isRegistration) {
+            setEmail('');
+            setPassword('');
+            setFirstname('');
+            setLastname('');
+            setImage(null);
+        } else {
+            setUsername('');
+            setPassword('');
+        }
+    }, [isRegistration]);
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post('http://localhost:8888/login', {
+                username: username,
+                password: password,
+            });
+
+            if (response.status === 200) {
+                const token = response.data.access_token;
+                localStorage.setItem('accessToken', token);
+                navigate('/statistics');
+                message.success('Успешная авторизация!');
+            } else {
+                message.error('Неверные учетные данные.');
+            }
+        } catch (error) {
+            console.error('Ошибка авторизации:', error);
+            message.error('Ошибка авторизации.');
+        }
     };
 
-    const handleRegister = () => {
-        console.log('Регистрация');
-        navigate('/profile');
+    const handleRegister = async () => {
+        try {
+            const formData = new FormData();
+            formData.append('userDTO', new Blob([JSON.stringify({
+                email: email,
+                password: password,
+                firstname: firstname,
+                lastname: lastname
+            })], {
+                type: "application/json"
+            }));
+
+            formData.append('image', image);
+
+            const response = await axios.post('http://localhost:8888/create', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+
+            if (response.status === 201) {
+                message.success('Регистрация прошла успешно! Теперь войдите.');
+                setIsRegistration(false);
+            } else {
+                message.error('Ошибка регистрации.');
+            }
+        } catch (error) {
+            console.error('Ошибка регистрации:', error);
+            message.error('Ошибка регистрации: ' + error.message);
+        }
+    };
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        setImage(file);
     };
 
     const handleForgotPassword = () => {
@@ -56,22 +124,102 @@ const AuthPage = () => {
                         {isRegistration ? 'Регистрация' : 'Авторизация'}
                     </h2>
                     <div className="auth-form">
-                        <div className="auth-field">
-                            <span className="auth-label">Логин:</span>
-                            <Input
-                                id="username"
-                                placeholder="Email"
-                                className="auth-input"
-                            />
-                        </div>
-                        <div className="auth-field">
-                            <span className="auth-label">Пароль:</span>
-                            <Input.Password
-                                id="password"
-                                placeholder="Password"
-                                className="auth-input"
-                            />
-                        </div>
+                        {!isRegistration && (
+                            <>
+                                <div className="auth-field">
+                                    <span className="auth-label">Username:</span>
+                                    <Input
+                                        id="username"
+                                        placeholder="Username"
+                                        className="auth-input"
+                                        value={username}
+                                        onChange={(e) => setUsername(e.target.value)}
+                                    />
+                                </div>
+                                <div className="auth-field">
+                                    <span className="auth-label">Пароль:</span>
+                                    <Input.Password
+                                        id="password"
+                                        placeholder="Password"
+                                        className="auth-input"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                    />
+                                </div>
+                            </>
+                        )}
+                        {isRegistration && (
+                            <>
+                                <div className="auth-field">
+                                    <span className="auth-label">Email:</span>
+                                    <Input
+                                        id="email"
+                                        placeholder="Email"
+                                        className="auth-input"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </div>
+                                <div className="auth-field">
+                                    <span className="auth-label">Пароль:</span>
+                                    <Input.Password
+                                        id="password"
+                                        placeholder="Password"
+                                        className="auth-input"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                    />
+                                </div>
+                                <div className="auth-field">
+                                    <span className="auth-label">Имя:</span>
+                                    <Input
+                                        id="firstname"
+                                        placeholder="First Name"
+                                        className="auth-input"
+                                        value={firstname}
+                                        onChange={(e) => setFirstname(e.target.value)}
+                                    />
+                                </div>
+                                <div className="auth-field">
+                                    <span className="auth-label">Фамилия:</span>
+                                    <Input
+                                        id="lastname"
+                                        placeholder="Last Name"
+                                        className="auth-input"
+                                        value={lastname}
+                                        onChange={(e) => setLastname(e.target.value)}
+                                    />
+                                </div>
+                                <div className="auth-field image-upload-container">
+                                    <span className="auth-label">Фотография:</span>
+                                    <label htmlFor="image-upload" className="image-upload-circle">
+                                        {image ? (
+                                            <img
+                                                src={URL.createObjectURL(image)}
+                                                alt="Preview"
+                                                style={{
+                                                    width: '100%',
+                                                    height: '100%',
+                                                    objectFit: 'cover',
+                                                    borderRadius: '50%',
+                                                }}
+                                            />
+                                        ) : (
+                                            <div className="upload-icon">
+                                                +
+                                            </div>
+                                        )}
+                                    </label>
+                                    <input
+                                        type="file"
+                                        id="image-upload"
+                                        accept="image/*"
+                                        onChange={handleImageChange}
+                                        style={{display: 'none'}}
+                                    />
+                                </div>
+                            </>
+                        )}
                         <div className="auth-button-container">
                             <Button
                                 type="primary"
